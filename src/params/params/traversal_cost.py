acafde96ef62049d741abbb809c5acc50f9ba562
Guillaume THOMAS (Whojo) <guillaume.thomas@ensta-paris.fr>
2023-09-22 14:01:11 +0200

    Add sand to plot

--- src/params/params/traversal_cost.py
+++ src/params/params/traversal_cost.py
@@ -6,16 +6,18 @@
 colors = {
     "road_easy": "black",
     "road_medium": "grey",
-    "forest_dirt_easy": "orangered",
+    "forest_dirt_easy": "deeppink",
     "dust": "orange",
     "forest_leaves": "firebrick",
-    "forest_dirt_medium": "darkgoldenrod",
+    "forest_dirt_medium": "navy",
     "gravel_easy": "dodgerblue",
     "grass_easy": "limegreen",
     "grass_medium": "darkgreen",
     "gravel_medium": "royalblue",
     "forest_leaves_branches": "indigo",
-    "forest_dirt_stones_branches": "purple",
+    "forest_dirt_stones_branches": "blueviolet",
+    "sand_hard": "darkgoldenrod",
+    "sand_medium": "orangered",
     }
 
 
