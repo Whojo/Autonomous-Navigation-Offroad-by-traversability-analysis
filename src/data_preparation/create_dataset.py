6af6efed21b797c7a61a082a9cc3f63b4143d2bc
Guillaume THOMAS (Whojo) <guillaume.thomas@ensta-paris.fr>
2023-10-11 16:48:47 +0200

    Refacto create_dataset

--- src/data_preparation/create_dataset.py
+++ src/data_preparation/create_dataset.py
@@ -65,6 +65,33 @@ import params.learning
 
 plt.rcParams["text.usetex"] = True  # Render Matplotlib text with Tex
 
+RectangleDim = namedtuple("RectangleDim", ["min_x", "max_x", "min_y", "max_y"])
+
+
+def _get_recursive_bag_files(files: list) -> list:
+    """
+    Recursively searches through a list of files and directories to find all bag files.
+
+    Args:
+        files (list): A list of file paths to search through.
+
+    Returns:
+        list: A list of all bag files found in the given files and directories.
+    """
+    bag_files = []
+    for file in files:
+        # Check if the file is a bag file
+        if os.path.isfile(file) and file.endswith(".bag"):
+            bag_files.append(file)
+
+        # If the path links to a directory, go through the files inside it
+        elif os.path.isdir(file):
+            bag_files.extend(
+                [file + f for f in os.listdir(file) if f.endswith(".bag")]
+            )
+
+    return bag_files
+
 
 def is_bag_healthy(bag: str) -> bool:
     """Check if a bag file is healthy
@@ -97,6 +124,29 @@ def is_bag_healthy(bag: str) -> bool:
     return True
 
 
+def _get_msg_depth(bag: rosbag.Bag, t_image: rospy.Time) -> np.array:
+    depth_list = list(
+        bag.read_messages(
+            topics=[params.robot.DEPTH_TOPIC],
+            start_time=t_image - rospy.Duration(params.dataset.TIME_DELTA),
+            end_time=t_image + rospy.Duration(params.dataset.TIME_DELTA),
+        )
+    )
+
+    min_t = params.dataset.TIME_DELTA
+    msg_depth = None
+
+    # Find the depth image whose timestamp is closest to that
+    # of the rgb image
+    for _, msg_depth_i, t_depth in depth_list:
+        new_t = np.abs(t_depth.to_sec() - t_image.to_sec())
+        if new_t < min_t:
+            min_t = new_t
+            msg_depth = msg_depth_i
+
+    return msg_depth
+
+
 def is_inside_image(image: np.ndarray, point: np.ndarray) -> bool:
     """Check if a point is inside an image
 
@@ -108,15 +158,25 @@ def is_inside_image(image: np.ndarray, point: np.ndarray) -> bool:
         bool: True if the point is inside the image, False otherwise
     """
     x, y = point
-    return (x >= 0) and (x < image.shape[1]) and (y >= 0) and (y < image.shape[0])
-
-
-RectangleDim = namedtuple("RectangleDim", ["min_x", "max_x", "min_y", "max_y"])
+    return (
+        (x >= 0) and (x < image.shape[1]) and (y >= 0) and (y < image.shape[0])
+    )
 
 
 def _get_outter_rectangle(
     points_image_old: np.array, points_image: np.array
 ) -> RectangleDim:
+    """
+    Given two arrays of points, returns the minimum bounding rectangle that contains all the points.
+
+    Args:
+    - points_image_old: np.array, shape (n, 2), containing the coordinates of the points in the old image.
+    - points_image: np.array, shape (n, 2), containing the coordinates of the points in the new image.
+
+    Returns:
+    - RectangleDim: a named tuple containing the dimensions of the minimum bounding rectangle.
+    """
+
     all_points = np.vstack((points_image_old, points_image))
 
     max_y = np.max(all_points[:, 1])
@@ -128,6 +188,17 @@ def _get_outter_rectangle(
 
 
 def _to_valid_patch_dimension(rec: RectangleDim) -> RectangleDim:
+    """
+    Converts a rectangle to a valid patch dimension by adjusting its width and height
+    based on the minimum width and rectangle ratio specified in the dataset parameters.
+
+    Args:
+        rec (RectangleDim): The rectangle to be converted.
+
+    Returns:
+        RectangleDim: The converted rectangle with valid patch dimensions.
+    """
+
     min_x, max_x, min_y, max_y = rec
 
     center_x = (min_x + max_x) / 2
@@ -241,28 +312,10 @@ class DatasetBuilder:
                 non-coherent velocity on the patch (i. e. extremum velocity are
                 too far from the mean). Default to True.
         """
-        # Initialize the index of the image and the trajectory
         index_image = 0
-        index_trajectory = 0
 
-        # Create a list to store bag files paths
-        bag_files = []
-
-        # Go through the list of files
-        for file in files:
-            # Check if the file is a bag file
-            if os.path.isfile(file) and file.endswith(".bag"):
-                bag_files.append(file)
-
-            # If the path links to a directory, go through the files inside it
-            elif os.path.isdir(file):
-                bag_files.extend(
-                    [file + f for f in os.listdir(file) if f.endswith(".bag")]
-                )
-
-        # Go through multiple bag files
+        bag_files = _get_recursive_bag_files(files)
         for file in tqdm(bag_files):
-            # Open the bag file
             bag = rosbag.Bag(file)
 
             # Check if the bag file is healthy (i.e. if it contains all the
@@ -272,47 +325,17 @@ class DatasetBuilder:
                 print("File " + file + " is incomplete. Skipping...")
                 continue
 
-            # Go through the image topic
             for _, msg_image, t_image in bag.read_messages(
                 topics=[params.robot.IMAGE_TOPIC]
             ):
-                # Define a variable to store the depth image
-                msg_depth = None
-
-                # Keep only the images that can be matched with a depth image
-                if list(
-                    bag.read_messages(
-                        topics=[params.robot.DEPTH_TOPIC],
-                        start_time=t_image - rospy.Duration(params.dataset.TIME_DELTA),
-                        end_time=t_image + rospy.Duration(params.dataset.TIME_DELTA),
-                    )
-                ):
-                    # Find the depth image whose timestamp is closest to that
-                    # of the rgb image
-                    min_t = params.dataset.TIME_DELTA
-
-                    # Go through the depth topic
-                    for _, msg_depth_i, t_depth in bag.read_messages(
-                        topics=[params.robot.DEPTH_TOPIC],
-                        start_time=t_image - rospy.Duration(params.dataset.TIME_DELTA),
-                        end_time=t_image + rospy.Duration(params.dataset.TIME_DELTA),
-                    ):
-                        # Keep the depth image whose timestamp is closest to
-                        # that of the rgb image
-                        if np.abs(t_depth.to_sec() - t_image.to_sec()) < min_t:
-                            min_t = np.abs(t_depth.to_sec() - t_image.to_sec())
-                            msg_depth = msg_depth_i
-
-                # If no depth image is found, skip the current image
-                else:
+                msg_depth = _get_msg_depth(bag, t_image)
+                if msg_depth is None:
                     continue
 
                 # Get the first odometry message received after the image
                 _, first_msg_odom, t_odom = next(
-                    iter(
-                        bag.read_messages(
-                            topics=[params.robot.ODOM_TOPIC], start_time=t_image
-                        )
+                    bag.read_messages(
+                        topics=[params.robot.ODOM_TOPIC], start_time=t_image
                     )
                 )
 
@@ -324,22 +347,19 @@ class DatasetBuilder:
                 ):
                     continue
 
-                # Convert the current ROS image to the OpenCV type
                 image = self.bridge.imgmsg_to_cv2(
                     msg_image, desired_encoding="passthrough"
                 )
-
-                # Convert the current ROS depth image to the OpenCV type
                 depth_image = self.bridge.imgmsg_to_cv2(
                     msg_depth, desired_encoding="passthrough"
                 )
 
-                # Compute the transform matrix between the world and the robot
                 WORLD_TO_ROBOT = frames.pose_to_transform_matrix(
                     first_msg_odom.pose.pose
                 )
-                # Compute the inverse transform
-                ROBOT_TO_WORLD = frames.inverse_transform_matrix(WORLD_TO_ROBOT)
+                ROBOT_TO_WORLD = frames.inverse_transform_matrix(
+                    WORLD_TO_ROBOT
+                )
 
                 # Define an array to store the previous front wheels
                 # coordinates in the image
@@ -352,13 +372,6 @@ class DatasetBuilder:
                 # world frame
                 point_world_old = None
 
-                # Define a variable to store the distance travelled by the
-                # robot between the current robot position and the previous
-                # one
-                distance = 0
-
-                # Create a list to store the robot velocities on the next
-                # rectangular patch
                 x_velocity = []
 
                 # Read the odometry measurement for T second(s)
@@ -380,8 +393,6 @@ class DatasetBuilder:
                             ]
                         ]
                     )
-
-                    # Make the orientation quaternion a numpy array
                     q = np.array(
                         [
                             msg_odom.pose.pose.orientation.x,
@@ -413,7 +424,9 @@ class DatasetBuilder:
 
                     # Gather front wheels outer points coordinates in
                     # a single array
-                    points_world = np.concatenate([point_left_world, point_right_world])
+                    points_world = np.concatenate(
+                        [point_left_world, point_right_world]
+                    )
 
                     # Compute the points coordinates in the robot frame
                     points_robot = frames.apply_rigid_motion(
@@ -463,9 +476,9 @@ class DatasetBuilder:
                     qup, qdown = np.percentile(x_velocity_array, [90, 10])
                     q_thresh = 0.08
 
-                    coherence = (qdown > np.mean(x_velocity) * (1.0 - q_thresh)) and (
-                        qup < np.mean(x_velocity) * (1.0 + q_thresh)
-                    )
+                    coherence = (
+                        qdown > np.mean(x_velocity) * (1.0 - q_thresh)
+                    ) and (qup < np.mean(x_velocity) * (1.0 + q_thresh))
                     cohesion = (
                         np.all(x_velocity_array > 0)
                         if x_velocity_array[0] > 0
@@ -485,7 +498,9 @@ class DatasetBuilder:
                         continue
 
                     # Compute the inclination of the patch in the image
-                    delta_old = np.abs(points_image_old[0] - points_image_old[1])
+                    delta_old = np.abs(
+                        points_image_old[0] - points_image_old[1]
+                    )
                     delta_current = np.abs(points_image[0] - points_image[1])
 
                     patch_angle = (
@@ -501,12 +516,16 @@ class DatasetBuilder:
 
                     if not is_inside_image(
                         image, (patch.max_x, patch.max_y)
-                    ) or not is_inside_image(image, (patch.min_x, patch.max_y)):
+                    ) or not is_inside_image(
+                        image, (patch.min_x, patch.max_y)
+                    ):
                         break
 
                     if not is_inside_image(
                         image, (patch.max_x, patch.min_y)
-                    ) or not is_inside_image(image, (patch.min_x, patch.min_y)):
+                    ) or not is_inside_image(
+                        image, (patch.min_x, patch.min_y)
+                    ):
                         continue
 
                     # Draw a rectangle in the image to visualize the region
@@ -526,11 +545,15 @@ class DatasetBuilder:
                     # cv2.imshow("rgb", image_to_save)
                     # cv2.waitKey(0)
 
-                    image_to_save = cv2.cvtColor(image_to_save, cv2.COLOR_BGR2RGB)
+                    image_to_save = cv2.cvtColor(
+                        image_to_save, cv2.COLOR_BGR2RGB
+                    )
 
                     image_to_save = Image.fromarray(image_to_save)
                     image_name = f"{index_image:05d}.png"
-                    image_to_save.save(self.images_directory + "/" + image_name, "PNG")
+                    image_to_save.save(
+                        self.images_directory + "/" + image_name, "PNG"
+                    )
 
                     # Extract the rectangular region of interest from
                     # the original depth image
@@ -538,7 +561,9 @@ class DatasetBuilder:
                         patch.min_y : patch.max_y, patch.min_x : patch.max_x
                     ]
 
-                    depth = Depth(depth_image_crop.copy(), params.dataset.DEPTH_RANGE)
+                    depth = Depth(
+                        depth_image_crop.copy(), params.dataset.DEPTH_RANGE
+                    )
                     depth.compute_normal(
                         K=params.robot.K,
                         bilateral_filter=params.dataset.BILATERAL_FILTER,
@@ -568,7 +593,9 @@ class DatasetBuilder:
                     )
 
                     # Convert the image from BGR to RGB
-                    normal_to_save = cv2.cvtColor(normal_to_save, cv2.COLOR_BGR2RGB)
+                    normal_to_save = cv2.cvtColor(
+                        normal_to_save, cv2.COLOR_BGR2RGB
+                    )
 
                     normal_to_save = Image.fromarray(normal_to_save)
                     normal_to_save.save(
@@ -590,14 +617,18 @@ class DatasetBuilder:
                         # Append angular velocities and vertical
                         # acceleration to the previously created lists
                         roll_velocity_values.append(msg_imu.angular_velocity.x)
-                        pitch_velocity_values.append(msg_imu.angular_velocity.y)
+                        pitch_velocity_values.append(
+                            msg_imu.angular_velocity.y
+                        )
                         vertical_acceleration_values.append(
                             msg_imu.linear_acceleration.z - 9.81
                         )
 
                     # Extract features from the IMU signals and fill the
                     # features array
-                    self.features[index_image] = traversalcost.utils.get_features(
+                    self.features[
+                        index_image
+                    ] = traversalcost.utils.get_features(
                         roll_velocity_values,
                         pitch_velocity_values,
                         vertical_acceleration_values,
@@ -630,9 +661,6 @@ class DatasetBuilder:
                     # cv2.imshow("Image", cv2.resize(image, (1280, 720)))
                     # cv2.waitKey()
 
-                # FIXME: to keep? (RNN)
-                index_trajectory += 1
-
             bag.close()
 
         # Keep only the rows that are not filled with zeros
@@ -642,7 +670,20 @@ class DatasetBuilder:
             np.any(self.pitch_rate_variance, axis=1)
         ]
 
-    def compute_traversal_costs(self):
+    def compute_traversal_costs(
+        self, plot_velocity_distribution: bool = False
+    ):
+        """
+        Computes the traversal costs for the given features using a Siamese Network model.
+        The costs are then discretized using K-means binning and the midpoints of the bins are saved in the dataset directory.
+        If plot_velocity_distribution is True, a histogram of the traversal costs is plotted.
+
+        Args:
+            plot_velocity_distribution (bool): Whether to plot a histogram of the traversal costs or not.
+
+        Returns:
+            tuple: A tuple containing the original traversal costs and the discretized traversal costs.
+        """
         # Load a model
         model = traversalcost.traversal_cost.SiameseNetwork(
             input_size=self.features_size
@@ -655,8 +696,6 @@ class DatasetBuilder:
             params=params.dataset.SIAMESE_PARAMS,
             device=params.dataset.DEVICE,
         )
-
-        # costs = self.pitch_rate_variance
 
         # Apply K-means binning
         discretizer = KBinsDiscretizer(
@@ -673,12 +712,13 @@ class DatasetBuilder:
         # Save the midpoints in the dataset directory
         np.save(self.dataset_directory + "/bins_midpoints.npy", bins_midpoints)
 
-        plt.figure()
-        plt.hist(costs, bins_edges, lw=1, ec="magenta", fc="blue")
-        plt.title("Traversal cost binning")
-        plt.xlabel("Traversal cost")
-        plt.ylabel("Samples")
-        plt.show()
+        if plot_velocity_distribution:
+            plt.figure()
+            plt.hist(costs, bins_edges, lw=1, ec="magenta", fc="blue")
+            plt.title("Traversal cost binning")
+            plt.xlabel("Traversal cost")
+            plt.ylabel("Samples")
+            plt.show()
 
         return costs, digitized_costs
 
@@ -741,9 +781,20 @@ class DatasetBuilder:
         # Close the csv file
         file_costs.close()
 
-    def create_train_test_splits(self) -> None:
+    def create_train_test_splits(
+        self, plot_velocity_distribution: bool = False
+    ) -> None:
         """
-        Split the dataset into training and testing sets.
+        Splits the dataset randomly into training and testing sets, creates sub-directories to store train and test images,
+        counts the number of samples per class, copies the images to the respective directories and stores the train and test
+        splits in csv files.
+
+        Args:
+        - self: instance of the CreateDataset class
+        - plot_velocity_distribution: boolean flag to plot the distribution of train and test sets
+
+        Returns:
+        - None
         """
         # Create a sub-directory to store train images
         train_directory = self.dataset_directory + "/images_train"
@@ -769,23 +820,31 @@ class DatasetBuilder:
         )
 
         # Count the number of samples per class
-        train_distribution = dataframe_train["traversability_label"].value_counts()
-        test_distribution = dataframe_test["traversability_label"].value_counts()
+        train_distribution = dataframe_train[
+            "traversability_label"
+        ].value_counts()
+        test_distribution = dataframe_test[
+            "traversability_label"
+        ].value_counts()
 
-        plt.bar(
-            train_distribution.index,
-            train_distribution.values,
-            fc="blue",
-            label="train",
-        )
-        plt.bar(
-            test_distribution.index, test_distribution.values, fc="orange", label="test"
-        )
-        plt.legend()
-        plt.title("Train and test sets distribution")
-        plt.xlabel("Traversability label")
-        plt.ylabel("Samples")
-        plt.show()
+        if plot_velocity_distribution:
+            plt.bar(
+                train_distribution.index,
+                train_distribution.values,
+                fc="blue",
+                label="train",
+            )
+            plt.bar(
+                test_distribution.index,
+                test_distribution.values,
+                fc="orange",
+                label="test",
+            )
+            plt.legend()
+            plt.title("Train and test sets distribution")
+            plt.xlabel("Traversability label")
+            plt.ylabel("Samples")
+            plt.show()
 
         # Iterate over each row of the training set and copy the images to the
         # training directory
