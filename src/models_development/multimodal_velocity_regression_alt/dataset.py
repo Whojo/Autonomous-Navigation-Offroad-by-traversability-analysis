e9a47f45ff12a8095ae78c46c08f9dcf009be462
Guillaume THOMAS (Whojo) <guillaume.thomas@ensta-paris.fr>
2023-10-13 12:20:00 +0200

    Refactor model training script
    Necessary for Hyperparameter tuning

--- src/models_development/multimodal_velocity_regression_alt/dataset.py
+++ src/models_development/multimodal_velocity_regression_alt/dataset.py
@@ -1,10 +1,16 @@
 import os
 import pandas as pd
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, Subset
 import torch
-import tifffile
 import numpy as np
+
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+
+from custom_transforms import Cutout, Shadowcasting
+import params.learning
+from params.learning import NORMALIZE_PARAMS, LEARNING
 
 
 class TraversabilityDataset(Dataset):
@@ -27,24 +33,21 @@ class TraversabilityDataset(Dataset):
 
         Args:
             traversal_costs_file (string): Path to the csv file which contains
-            images index and their associated traversal cost
+                images index and their associated traversal cost
             images_directory (string): Directory with all the images
             transform_image (callable, optional): Transforms to be applied on a
-            rdg image. Defaults to None.
+                rdg image. Defaults to None.
             transform_depth (callable, optional): Transforms to be applied on a
-            depth image. Defaults to None.
-            transform_normal (callable, optional): Transforms to be applied on a
-            normal image. Defaults to None.
+                depth image. Defaults to None.
+            transform_normal (callable, optional): Transforms to be applied on
+                a normal image. Defaults to None.
         """
-        # Read the csv file
         self.traversal_costs_frame = pd.read_csv(
             traversal_costs_file, converters={"image_id": str}
         )
 
-        # Initialize the name of the images directory
         self.images_directory = images_directory
 
-        # Initialize the transforms
         self.transform_image = transform_image
         self.transform_depth = transform_depth
         self.transform_normal = transform_normal
@@ -55,8 +58,6 @@ class TraversabilityDataset(Dataset):
         Returns:
             int: Number of samples
         """
-        # Count the number of files in the image directory
-        # return len(os.listdir(self.images_directory))
         return len(self.traversal_costs_frame)
 
     def __getitem__(self, idx: int) -> tuple:
@@ -72,47 +73,141 @@ class TraversabilityDataset(Dataset):
               traversability_label,
               linear_velocity])
         """
-        # Get the image name at index idx
         image_name = os.path.join(
             self.images_directory,
             self.traversal_costs_frame.loc[idx, "image_id"],
         )
 
-        # Read the image
         image = Image.open(image_name + ".png")
-
-        # Eventually apply transforms to the image
         if self.transform_image:
             image = self.transform_image(image)
 
-        # Read the depth image
-        # depth_image = tifffile.imread(image_name + "d.tiff")
         depth_image = Image.open(image_name + "d.png")
-
-        # Eventually apply transforms to the depth image
         if self.transform_depth:
             depth_image = self.transform_depth(depth_image)
 
-        # Read the normal map
-        # normal_map = tifffile.imread(image_name + "n.tiff")
         normal_map = Image.open(image_name + "n.png")
-
-        # Eventually apply transforms to the normal map
         if self.transform_normal:
             normal_map = self.transform_normal(normal_map)
 
-        # Concatenate the rgb, depth and normal images
-        # multimodal_image = torch.cat((image, depth_image)).float()
         multimodal_image = torch.cat((image, depth_image, normal_map)).float()
 
-        # Get the corresponding traversal cost
         traversal_cost = self.traversal_costs_frame.loc[
             idx, "traversal_cost"
         ].astype(np.float32)
-
-        # Get the corresponding linear velocity
         linear_velocity = self.traversal_costs_frame.loc[
             idx, "linear_velocity"
         ]
 
         return multimodal_image, traversal_cost, linear_velocity
+
+
+train_transform = transforms.Compose(
+    [
+        transforms.Resize(params.learning.IMAGE_SHAPE, antialias=True),
+        # transforms.RandomHorizontalFlip(p=0.5),
+        # transforms.RandomCrop(100),
+        transforms.ColorJitter(**params.learning.JITTER_PARAMS),
+        # Black patch
+        Cutout(0.5),
+        Shadowcasting(0.5),
+        transforms.ToTensor(),
+        # Gaussian noise
+        transforms.Lambda(lambda x: x + (0.001**0.5) * torch.randn(x.shape)),
+        transforms.Normalize(
+            mean=NORMALIZE_PARAMS["rbg"]["mean"],
+            std=NORMALIZE_PARAMS["rbg"]["std"],
+        ),
+    ]
+)
+
+test_transform = transforms.Compose(
+    [
+        transforms.Resize(params.learning.IMAGE_SHAPE, antialias=True),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=NORMALIZE_PARAMS["rbg"]["mean"],
+            std=NORMALIZE_PARAMS["rbg"]["std"],
+        ),
+    ]
+)
+
+transform_depth = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize(params.learning.IMAGE_SHAPE, antialias=True),
+        transforms.Normalize(
+            mean=NORMALIZE_PARAMS["depth"]["mean"],
+            std=NORMALIZE_PARAMS["depth"]["std"],
+        ),
+    ]
+)
+
+transform_normal = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize(params.learning.IMAGE_SHAPE, antialias=True),
+        transforms.Normalize(
+            mean=NORMALIZE_PARAMS["normal"]["mean"],
+            std=NORMALIZE_PARAMS["normal"]["std"],
+        ),
+    ]
+)
+
+
+train_set = TraversabilityDataset(
+    traversal_costs_file=params.learning.DATASET / "traversal_costs_train.csv",
+    images_directory=params.learning.DATASET / "images_train",
+    transform_image=train_transform,
+    transform_depth=transform_depth,
+    transform_normal=transform_normal,
+)
+
+val_set = TraversabilityDataset(
+    traversal_costs_file=params.learning.DATASET / "traversal_costs_train.csv",
+    images_directory=params.learning.DATASET / "images_train",
+    transform_image=test_transform,
+    transform_depth=transform_depth,
+    transform_normal=transform_normal,
+)
+
+test_set = TraversabilityDataset(
+    traversal_costs_file=params.learning.DATASET / "traversal_costs_test.csv",
+    images_directory=params.learning.DATASET / "images_test",
+    transform_image=test_transform,
+    transform_depth=transform_depth,
+    transform_normal=transform_normal,
+)
+
+train_size = params.learning.TRAIN_SIZE / (1 - params.learning.TEST_SIZE)
+train_indices, val_indices = train_test_split(
+    range(len(train_set)), train_size=train_size
+)
+
+train_set = Subset(train_set, train_indices)
+val_set = Subset(val_set, val_indices)
+
+
+def get_loader(set: Dataset, *, shuffle: bool = True) -> DataLoader:
+    """Return a DataLoader for a given dataset
+
+    Args:
+        set (Dataset): Dataset to load
+        shuffle (bool, optional): Whether to shuffle the data. Defaults to
+            True.
+
+    Returns:
+        DataLoader: DataLoader for the given dataset
+    """
+    return DataLoader(
+        set,
+        batch_size=LEARNING["batch_size"],
+        shuffle=shuffle,
+        num_workers=12,  # Asynchronous data loading and augmentation
+        pin_memory=True,  # Increase the transferring speed of the data to the GPU
+    )
+
+
+train_loader = get_loader(train_set)
+val_loader = get_loader(val_set)
+test_loader = get_loader(test_set, shuffle=False)
--- src/models_development/multimodal_velocity_regression_alt/test.py
+++ src/models_development/multimodal_velocity_regression_alt/test.py
@@ -4,26 +4,28 @@ import torch.nn.functional as F
 import numpy as np
 
 
-def squared_error(predicted_costs: torch.Tensor,
-                  true_costs: torch.Tensor) -> torch.Tensor:
+def squared_error(
+    predicted_costs: torch.Tensor, true_costs: torch.Tensor
+) -> torch.Tensor:
     """
     Apply the squared error function to a predicted and true traversal cost
-    
+
     Args:
         predicted_costs (Tensor): Predicted traversal cost
         true_costs (Tensor): True traversal cost
-        
+
     Returns:
         Tensor: The squared error between the predicted and true traversal cost
     """
-    return (predicted_costs - true_costs)**2
+    return (predicted_costs - true_costs) ** 2
 
 
-def test(model: nn.Module,
-         device: str,
-         test_loader: torch.utils.data.DataLoader,
-         criterion_regression: nn.Module,
-         uncertainty_function: callable) -> tuple:
+def test(
+    model: nn.Module,
+    device: str,
+    test_loader: torch.utils.data.DataLoader,
+    criterion_regression: nn.Module,
+) -> tuple:
     """Test the model on the test set
 
     Args:
@@ -37,17 +39,13 @@ def test(model: nn.Module,
         regression task
         bins_midpoints (np.ndarray): The midpoints of the bins used for the
         discretization of the traversal cost
-        uncertainty_function (callable): The function to use to compute the
-        uncertainty
 
     Returns:
         double, double, double, list, list: The average loss, the accuracy,
         the regression loss, the list of the uncertainties, the list of
         regression losses
     """
-
-    # Testing
-    test_regression_loss = 0.
+    test_regression_loss = 0.0
 
     # Configure the model for testing
     model.eval()
@@ -56,45 +54,45 @@ def test(model: nn.Module,
 
     with torch.no_grad():
         # Loop over the testing batches
-        for images,\
-            traversal_costs,\
-            linear_velocities in test_loader:
-
+        for images, traversal_costs, linear_velocities in test_loader:
             images = images.to(device)
             traversal_costs = traversal_costs.to(device)
-            linear_velocities =\
-                linear_velocities.type(torch.float32).to(device)
+            linear_velocities = linear_velocities.type(torch.float32).to(
+                device
+            )
 
             # Add a dimension to the linear velocities tensor
             linear_velocities.unsqueeze_(1)
-            
+
             # Perform forward pass
             predicted_traversability_score = model(images, linear_velocities)
 
             # Compute and accumulate the batch loss
             test_regression_loss += criterion_regression(
-                predicted_traversability_score,
-                traversal_costs).item()
+                predicted_traversability_score, traversal_costs
+            ).item()
 
             # Compute the loss for each sample
             test_regression_losses.append(
-                squared_error(predicted_traversability_score,
-                              traversal_costs).to("cpu"))
+                squared_error(
+                    predicted_traversability_score, traversal_costs
+                ).to("cpu")
+            )
 
     # Compute the loss and accuracy
     test_regression_loss /= len(test_loader)
-   
-    return test_regression_loss,\
-           test_regression_losses
+
+    return test_regression_loss, test_regression_losses
 
 
-def test_models(models: list,
-                device: str,
-                test_loader: torch.utils.data.DataLoader,
-                criterion_classification: nn.Module,
-                criterion_regression: nn.Module,
-                bins_midpoints: np.ndarray,
-                uncertainty_function: callable) -> tuple:
+def test_models(
+    models: list,
+    device: str,
+    test_loader: torch.utils.data.DataLoader,
+    criterion_classification: nn.Module,
+    criterion_regression: nn.Module,
+    bins_midpoints: np.ndarray,
+) -> tuple:
     """Test the models on the test set
 
     Args:
@@ -108,8 +106,6 @@ def test_models(models: list,
         task
         bins_midpoints (np.ndarray): The midpoints of the bins used for the
         discretization of the traversal cost
-        uncertainty_function (callable): The function to use to compute the
-        uncertainty
 
     Returns:
         double, double, double, list, list: The average loss, the accuracy, the
@@ -117,7 +113,7 @@ def test_models(models: list,
         losses
     """
     # Testing
-    test_regression_loss = 0.
+    test_regression_loss = 0.0
 
     # Configure the models for testing
     for model in models:
@@ -127,68 +123,76 @@ def test_models(models: list,
     uncertainties = []
 
     with torch.no_grad():
-        
         # Loop over the testing batches
-        for images,\
-            traversal_costs,\
-            traversability_labels,\
-            linear_velocities in test_loader:
-
+        for (
+            images,
+            traversal_costs,
+            traversability_labels,
+            linear_velocities,
+        ) in test_loader:
             images = images.to(device)
             traversal_costs = traversal_costs.to(device)
-            linear_velocities =\
-                linear_velocities.type(torch.float32).to(device)
-            
+            linear_velocities = linear_velocities.type(torch.float32).to(
+                device
+            )
+
             # Add a dimension to the linear velocities tensor
             linear_velocities = linear_velocities.unsqueeze_(1)
 
             # expected_traversal_costs = torch.zeros(traversal_costs.shape[0], 1).to(device)
-            
+
             # uncert = torch.zeros(traversal_costs.shape[0]).to(device)
-            
-            probabilities = torch.zeros(traversal_costs.shape[0], 10).to(device)
-            
-            exp_costs = torch.zeros(traversal_costs.shape[0], len(models)).to(device)
-            
+
+            probabilities = torch.zeros(traversal_costs.shape[0], 10).to(
+                device
+            )
+
+            exp_costs = torch.zeros(traversal_costs.shape[0], len(models)).to(
+                device
+            )
+
             # Perform forward pass
             for index, model in enumerate(models):
-                
-                predicted_traversability_labels = model(images, linear_velocities)
+                predicted_traversability_labels = model(
+                    images, linear_velocities
+                )
 
                 # Apply the softmax function to the predicted traversability labels
-                probabilities += nn.Softmax(dim=1)(predicted_traversability_labels)
+                probabilities += nn.Softmax(dim=1)(
+                    predicted_traversability_labels
+                )
 
                 # Compute the expected traversal cost over the bins
                 # expected_traversal_costs += torch.matmul(probabilities, bins_midpoints)
-                
-                # Compute the uncertainty
-                # uncert += uncertainty_function(probabilities)
-                
+
                 exp_costs[:, index] = torch.matmul(
                     nn.Softmax(dim=1)(predicted_traversability_labels),
-                    bins_midpoints)[:, 0]
-            
+                    bins_midpoints,
+                )[:, 0]
+
             probabilities /= len(models)
-            
+
             # Compute the expected traversal cost over the bins
-            expected_traversal_costs = torch.matmul(probabilities, bins_midpoints)
-            
+            expected_traversal_costs = torch.matmul(
+                probabilities, bins_midpoints
+            )
+
             variance = torch.var(exp_costs, dim=1)
             uncertainties.append(variance.to("cpu"))
-            
-            # uncertainties.append(uncertainty_function(probabilities).to("cpu"))
-                
+
             # Compute and accumulate the batch loss
-            test_regression_loss +=\
-                criterion_regression(expected_traversal_costs[:, 0],
-                                     traversal_costs).item()
+            test_regression_loss += criterion_regression(
+                expected_traversal_costs[:, 0], traversal_costs
+            ).item()
 
             # Compute the loss for each sample
             test_regression_losses.append(
-                squared_error(expected_traversal_costs[:, 0],
-                              traversal_costs).to("cpu"))
+                squared_error(
+                    expected_traversal_costs[:, 0], traversal_costs
+                ).to("cpu")
+            )
 
     # Compute the regression loss
     test_regression_loss /= len(test_loader)
-   
+
     return test_regression_loss, test_regression_losses, uncertainties
