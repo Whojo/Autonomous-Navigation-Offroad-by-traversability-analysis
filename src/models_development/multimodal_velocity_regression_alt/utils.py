96e53dec1d708075b194f9e21431e341401deaec
Guillaume THOMAS (Whojo) <guillaume.thomas@ensta-paris.fr>
2023-10-13 11:41:03 +0200

    format utils

--- src/models_development/multimodal_velocity_regression_alt/utils.py
+++ src/models_development/multimodal_velocity_regression_alt/utils.py
@@ -7,8 +7,9 @@ import params.learning
 from dataset import TraversabilityDataset
 
 
-def compute_mean_std(images_directory: str,
-                     traversal_costs_file: str) -> tuple:
+def compute_mean_std(
+    images_directory: str, traversal_costs_file: str
+) -> tuple:
     """Compute the mean and standard deviation of the images of the dataset
 
     Args:
@@ -19,47 +20,50 @@ def compute_mean_std(images_directory: str,
     Returns:
         tuple: Mean and standard deviation of the dataset
     """
-    transform = transforms.Compose([
-        # Convert a PIL Image or numpy.ndarray to tensor
-        transforms.ToTensor(),
-        
-        # Reduce the size of the images
-        # (if size is an int, the smaller edge of the
-        # image will be matched to this number and the ration is kept)
-        transforms.Resize(params.learning.IMAGE_SHAPE),
-    ])
-     
+    transform = transforms.Compose(
+        [
+            # Convert a PIL Image or numpy.ndarray to tensor
+            transforms.ToTensor(),
+            # Reduce the size of the images
+            # (if size is an int, the smaller edge of the
+            # image will be matched to this number and the ration is kept)
+            transforms.Resize(params.learning.IMAGE_SHAPE),
+        ]
+    )
+
     # Create a Dataset for training
     dataset = TraversabilityDataset(
         traversal_costs_file=params.learning.DATASET / traversal_costs_file,
         images_directory=params.learning.DATASET / images_directory,
         transform_image=transform,
         transform_depth=transform,
-        transform_normal=transform
+        transform_normal=transform,
     )
-     
+
     loader = DataLoader(
         dataset=dataset,
         batch_size=1,
         shuffle=False,
         num_workers=12,
-        pin_memory=True
+        pin_memory=True,
     )
 
     cnt = 0
     first_moment = torch.empty(7)
     second_moment = torch.empty(7)
 
-    for images, traversal_costs, traversability_labels, linear_velocity in loader:
+    for images, _, _ in loader:
         b, c, h, w = images.shape
         nb_pixels = b * h * w
         sum_ = torch.sum(images, dim=[0, 2, 3])
-        sum_of_square = torch.sum(images ** 2, dim=[0, 2, 3])
+        sum_of_square = torch.sum(images**2, dim=[0, 2, 3])
         first_moment = (cnt * first_moment + sum_) / (cnt + nb_pixels)
-        second_moment = (cnt * second_moment + sum_of_square) / (cnt + nb_pixels)
+        second_moment = (cnt * second_moment + sum_of_square) / (
+            cnt + nb_pixels
+        )
         cnt += nb_pixels
 
     mean = first_moment
-    std = torch.sqrt(second_moment - first_moment ** 2)
-    
+    std = torch.sqrt(second_moment - first_moment**2)
+
     return mean, std
