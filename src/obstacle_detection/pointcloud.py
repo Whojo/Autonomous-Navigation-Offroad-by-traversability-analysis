3da77ef1a17d3c0a13889c027403ed3700b0be9d
abhaydmathur <admathur@iitk.ac.in>
2023-11-24 12:35:13 +0100

    backup

---
+++ src/obstacle_detection/pointcloud.py
@@ -0,0 +1,11 @@
+"""
+TODO @abhaydmathur 
+    1. Isolate obstacles from the pointcloud using
+        - RANSAC or <something else> to ID Surface Normals
+        - 
+"""
+
+import numpy as np
+
+def pointcloud_from_depth(depth):
+    pass
